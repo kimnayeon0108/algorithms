d7ad7b1899012fb9063a92d91ced661b85669b75
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-01-13 21:55:29 +0900

    Add LeetCode1.java

---
+++ LeetCod1.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class LeetCod1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 1; j < nums.length; j++){
+                if(target == nums[i] + nums[j] && i != j){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return null;
+    }
+}
