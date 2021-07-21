88f46c5e9a4fcaaa544491654ddc399a9771f18e
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-07-21 19:09:55 +0900

    Leet code 1512번 문제 풀이 https://leetcode.com/problems/number-of-good-pairs/

---
+++ leet-code/pass/NumberOfGoodPairs.java
@@ -0,0 +1,21 @@
+package pass;
+
+public class NumberOfGoodPairs {
+
+    public static int numIdenticalPairs(int[] nums) {
+
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numIdenticalPairs(new int[]{1, 2, 3, 1, 1, 3}));
+    }
+}
