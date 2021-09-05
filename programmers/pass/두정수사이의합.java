a95c3dd1285a1e543e9bc28be28e16d0e2253e22
Nayeon <kimnayeon0108@users.noreply.github.com>
2021-09-05 18:20:43 +0900

    프로그래머스 두 정수 사이의 합 문제풀이

---
+++ programmers/pass/두정수사이의합.java
@@ -0,0 +1,18 @@
+package pass;
+
+public class 두정수사이의합 {
+    public long solution(int a, int b) {
+        long sum = 0;
+        if(a>b){
+            int temp = a;
+            a = b;
+            b = temp;
+        } else if(a == b){
+            return a;
+        }
+        for(int i = a; i <= b; i++){
+            sum += i;
+        }
+        return sum;
+    }
+}
