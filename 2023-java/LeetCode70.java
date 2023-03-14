703752c5f4f6885236c4b878194f5966fdcd565f
kimnayeon0108@gmail.com <kimnayeon0108@users.noreply.github.com>
2023-03-14 20:24:00 +0900

    leetcode 70 Climbiing Stairs

---
+++ 2023-java/LeetCode70.java
@@ -0,0 +1,15 @@
+public class LeetCode70 {
+    public static int climbStairs(int n) {
+        int[] arr = new int[45];
+        arr[0] = 1;
+        arr[1] = 2;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 2] + arr[i - 1];
+        }
+        return arr[n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(2));
+    }
+}
